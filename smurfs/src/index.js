8c561850b80667c017c4160eebe9e024a56d4717
ajfrierson <alvinfrierson@fuller.edu>
2019-02-01 10:00:43 -0800

    added all of the needed dependencies

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import rootReducer from  './reducers';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const store = createStore(
   () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
