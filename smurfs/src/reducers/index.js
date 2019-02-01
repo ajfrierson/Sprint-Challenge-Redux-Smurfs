825f044e4c3a71619f0055f0c0a17cc553c0bd52
ajfrierson <alvinfrierson@fuller.edu>
2019-02-01 10:32:52 -0800

    built initialState functionality. It now included a state for each case: loading, get_smurfs and error.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -30,3 +30,15 @@ cost initialState = {
   error: ''
 }
 
+export default function (state = initialState, action) {
+  switch(action.type) {
+    case LOADING: 
+      return Object.assign({}, state, {loading: true })
+    case GET_SMURFS:
+      return Object.assign({}, state, { smurfs: action.smurfs, loading: false})
+    case ERROR:
+      return Object.assign({}, state, { error: action.errorMessage, loading: false})
+    default:
+      return state   
+  }
+}
